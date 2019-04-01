ac3541d678ad11694db38f7f9c97f516905692bf
TomaszBorys <49066569+TomaszBorys@users.noreply.github.com>
2019-04-01 20:11:12 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            hhhhddddhaaa <b>dddd</b>  Edit <code>src/App.js</code> and save to reload.
+            <b>dddd</b>  Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
